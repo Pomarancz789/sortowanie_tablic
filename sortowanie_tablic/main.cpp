91b992fd74af58d3f6c7c581eba52469db11b4b7
Pomarancz <kuba.wtorkiewicz@gmail.com>
2021-12-08 10:30:05 +0100

    przerobiłem na funkcje, i dodałem wersje bez swapa

--- sortowanie_tablic/main.cpp
+++ sortowanie_tablic/main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 using namespace std;
 
-
-int main() {
+void pierwsze()
+{
     int tab[5]={9,3,8,2,6};
     
     for (int i=0; i<5; i++)
@@ -19,6 +19,42 @@ int main() {
     {
         cout << tab[z] <<endl;
     }
+    
+}
+
+void drugie()
+{
+    int temp=0;
+    
+    int tab[5]={9,3,8,2,6};
+    
+    for (int i=0; i<5; i++)
+    {
+        for (int j=0; j<5; j++)
+        {
+            if (tab[i]<tab[j])
+                {
+                    temp=tab[i];
+                    tab[i]=tab[j];
+                    tab[j]=temp;
+                }
+        }
+    }
+    
+    
+    for (int z=0; z<5; z++)
+    {
+        cout << tab[z] <<endl;
+    }
+    
+}
+
+
+int main() {
+    
+    pierwsze();
+    drugie();
+    
     
     return 0;
 }
