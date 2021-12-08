4102f1560bd7893bef8506c917e30c16c5072caf
Pomarancz <kuba.wtorkiewicz@gmail.com>
2021-12-08 10:14:00 +0100

    yyyy, dziala sortowanie ale chyba nie do konca optymalnie

--- sortowanie_tablic/main.cpp
+++ sortowanie_tablic/main.cpp
@@ -3,8 +3,22 @@ using namespace std;
 
 
 int main() {
+    int tab[5]={9,3,8,2,6};
+    
+    for (int i=0; i<5; i++)
+    {
+        for (int j=0; j<5; j++)
+        {
+            if (tab[i]<tab[j])
+                swap(tab[i], tab[j]);
+        }
+    }
     
     
+    for (int z=0; z<5; z++)
+    {
+        cout << tab[z] <<endl;
+    }
     
     return 0;
 }
